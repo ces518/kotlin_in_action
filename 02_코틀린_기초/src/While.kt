1586e4075a039f0ff6af0b192e69caef2a29edde
ncucu <ncucu.me@kakaocommerce.com>
2020-09-23 18:08:39 +0900

    While

---
+++ 02_코틀린_기초/src/While.kt
@@ -0,0 +1,12 @@
+/**
+ * 코틀린에는 while, do-while이 있으며 자바와 동일한 기능을 제공한다..
+ */
+fun main(args: Array<String>) {
+    while (true) {
+        /*...*/
+    }
+
+    do {
+        /*...*/
+    } while (true)
+}
