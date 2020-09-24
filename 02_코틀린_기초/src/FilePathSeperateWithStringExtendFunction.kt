3534a48c3b24ac2c1286d0e00c642d5abaf2ecbf
ncucu <ncucu.me@kakaocommerce.com>
2020-09-25 00:05:43 +0900

    문자열 확장함수를 이용한 파일 경로 파싱

---
+++ 02_코틀린_기초/src/FilePathSeperateWithStringExtendFunction.kt
@@ -0,0 +1,15 @@
+/**
+ * String 확장 함수를 사용하여 경로 파싱
+ */
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+
+    val fileName = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+    println("Dir: $directory, name: $fileName, ext: $extension")
+}
+
+fun main(args: Array<String>) {
+    parsePath("/Users/yole/kotlin-book/chapter.adoc")
+}
